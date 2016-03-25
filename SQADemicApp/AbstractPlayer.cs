c0996b8fdab0d770ef3bd15a040ada6431db7d18
Jackie Zhang <zxq00zxq001@gmail.com>
2016-03-25 12:28:32 -0400

    [Step 4] Moved all methods from PA to AP. Need to fix dependencies.

--- SQADemicApp/AbstractPlayer.cs
+++ SQADemicApp/AbstractPlayer.cs
@@ -98,6 +98,178 @@ namespace SQADemicApp
             return options;
         }
 
+        /// <summary>
+        /// Moves the player to the given city, updating the hand if needed
+        /// </summary>
+        /// <param name="city">City to move to</param>
+        /// <returns>Success Flag/returns>
+        public bool MovePlayer(City city)
+        {
+            // TODO: Further refactoring is possible but has less priority.
+            if (DriveOptions(currentCity).Any(c => c.Name.Equals(city.Name)))
+            {
+                //Do Nothing
+            }
+            else if (ShuttleFlightOption(currentCity).Contains(city.Name))
+            {
+                //Do Nothing
+            }
+            else if (DirectFlightOption(hand, currentCity).Contains(city.Name))
+            {
+                hand.RemoveAll(x => x.CityName.Equals(city.Name));
+            }
+            else if (CharterFlightOption(hand, currentCity))
+            {
+                hand.RemoveAll(x => x.CityName.Equals(currentCity.Name));
+            }
+            else
+            {
+                return false;
+            }
+            currentCity = city;
+            return true;
+        }
+
+        /// <summary>
+        /// Moves the player to the destination city
+        /// </summary>
+        /// <param name="players">List of Players</param>
+        /// <param name="destination">Name of the City to be moved to</param>
+        /// <returns>Status Flag</returns>
+        public bool DispatcherMovePlayer(List<Player> players, City destination)
+        {
+            if (DriveOptions(currentCity).Any(p => p.Name.Equals(destination.Name)))
+            {
+                //Do nothing
+            }
+            else if (players.Any(p => p.currentCity.Name.Equals(destination.Name)))
+            {
+                //Do nothing
+            }
+            else if (ShuttleFlightOption(currentCity).Contains(destination.Name))
+            {
+                //Do Nothing
+            }
+            else {
+                return false;
+            }
+            currentCity = destination;
+            return true;
+        }
+
+        /// <summary>
+        ///  Builds a Research Station should the conditions be meet
+        /// </summary>
+        /// <returns>Success Flag</returns>
+        public bool BuildAResearchStationOption()
+        {
+            if (CityBL.getCitiesWithResearchStations().Contains(currentCity))
+                return false;
+            if (role == ROLE.OpExpert)
+            {
+                currentCity.researchStation = true;
+                return true;
+            }
+            if (hand.Any(c => c.CityName.Equals(currentCity.Name)))
+            {
+                hand.RemoveAll(x => x.CityName.Equals(currentCity.Name));
+                currentCity.researchStation = true;
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Cures the color if possible
+        /// </summary>
+        /// <param name="cardsToSpend"></param>
+        /// <param name="color"></param>
+        /// <returns>Success Flag</returns>
+        public bool Cure(List<String> cardsToSpend, COLOR color)
+        {
+            if (!currentCity.researchStation || GameBoardModels.CURESTATUS.GetCureStatus(color) != Cures.CURESTATE.NotCured)
+                return false;
+            var cards = hand.Where(x => x.CityColor == color && cardsToSpend.Contains(x.CityName));
+            if (role == ROLE.Scientist)
+            {
+                if (cards.Count() != 4)
+                    return false;
+            }
+            else if (cards.Count() != 5)
+                return false;
+            hand.RemoveAll(x => cards.Contains(x));
+            GameBoardModels.CURESTATUS.SetCureStatus(color, Cures.CURESTATE.Cured);
+            if (GameBoardModels.CURESTATUS.GetCureStatus(COLOR.black) != Cures.CURESTATE.NotCured &&
+                   GameBoardModels.CURESTATUS.GetCureStatus(COLOR.blue) != Cures.CURESTATE.NotCured &&
+                   GameBoardModels.CURESTATUS.GetCureStatus(COLOR.red) != Cures.CURESTATE.NotCured &&
+                   GameBoardModels.CURESTATUS.GetCureStatus(COLOR.yellow) != Cures.CURESTATE.NotCured)
+            {
+                throw new InvalidOperationException("Game Over You Win");
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Helper method for Treat Disease Option
+        /// </summary>
+        /// <param name="city"></param>
+        /// <param name="color"></param>
+        /// <returns>number of disease cubes in the city</returns>
+        private static int GetDiseaseCubes(City city, COLOR color)
+        {
+            return city.Cubes.GetCubeCount(color);
+        }
+
+        /// <summary>
+        /// Helper method for Treat Disease Option
+        /// </summary>
+        /// <param name="city"></param>
+        /// <param name="color"></param>
+        /// <param name="numberAfterCure"></param>
+        /// <param name="numberBeforeCure"></param>
+        private static bool SetDiseaseCubes(City city, COLOR color, int numberAfterCure, int numberBeforeCure)
+        {
+            numberAfterCure = GameBoardModels.CURESTATUS.GetCureStatus(color) == Cures.CURESTATE.Cured ? 0 : numberAfterCure;
+            GameBoardModels.cubeCount.AddCubes(color, numberBeforeCure - numberAfterCure);
+            city.Cubes.SetCubeCount(color, numberAfterCure);
+            if (GameBoardModels.cubeCount.GetCubeCount(color) == MAXCUBECOUNT && GameBoardModels.CURESTATUS.GetCureStatus(color) == Cures.CURESTATE.Cured)
+                GameBoardModels.CURESTATUS.SetCureStatus(color, Cures.CURESTATE.Sunset);
+            return true;
+        }
+
+        /// <summary>
+        /// Treates the Diesease if possible
+        /// </summary>
+        /// <param name="player"></param>
+        /// <param name="color"></param>
+        /// <returns>Success Flag</returns>
+        public bool TreatDiseaseOption(COLOR color)
+        {
+            int number = GetDiseaseCubes(currentCity, color);
+            if (number < 1)
+                return false;
+            return SetDiseaseCubes(currentCity, color, role == ROLE.Medic ? 0 : (number - 1), number);
+        }
+
+        /// <summary>
+        /// Allows Players to Trade Cards (from current player to receiver)
+        /// </summary>
+        /// <param name="reciver">Player who will recive the card</param>
+        /// <param name="cityname">Name on the card to be traded</param>
+        /// <returns>Sucess Flag</returns>
+        public bool ShareKnowledgeOption(Player reciver, string cityname)
+        {
+            if (currentCity != reciver.currentCity ||
+                (!reciver.currentCity.Name.Equals(cityname) && role != ROLE.Researcher))
+                return false;
+            int index = hand.FindIndex(x => x.CityName.Equals(cityname));
+            if (index == -1)
+                return false;
+            reciver.hand.Add(hand[index]);
+            hand.RemoveAt(index);
+            return true;
+        }
 
     }
 }
