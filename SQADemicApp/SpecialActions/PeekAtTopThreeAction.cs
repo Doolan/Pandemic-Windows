a982412f8acd47f52c3cd38da4a64e1c0893d4cc
unknown <lehmandr@LEHMANDR-1.rose-hulman.edu>
2016-04-20 06:52:26 -0400

    finished up modifying the behavior of Direct flight and the addition of the new action

---
+++ SQADemicApp/SpecialActions/PeekAtTopThreeAction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SQADemicApp.SpecialActions
+{
+    class PeekAtTopThreeAction : AbstractSpecialAction
+    {
+        public override bool PreformAction()
+        {
+            Stack<Card> deck = GameBoardModels.playerDeck;
+            String cards = "";
+            while (deck.Count != 0)
+            {
+                cards = cards + deck.Pop().ToString() + "\n";
+            }
+            MessageBox.Show("Top 3 Cards", cards, MessageBoxButtons.OK, MessageBoxIcon.None);
+            return true;
+        }
+
+    }
+}
